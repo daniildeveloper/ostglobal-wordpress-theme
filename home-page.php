9ef224f4b4864cf48e41fe4db47862d2bc9de269
daniildeveloper <daniildeveloper@ya.ru>
2018-07-09 09:14:25 +0600

    Base theme release

--- home-page.php
+++ home-page.php
@@ -10,32 +10,34 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package daniildeveloper-ostglobal
+ *
+ * Template: Home page
  */
 
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        <?php
+        while ( have_posts() ) :
+            the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+            get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
 
-		endwhile; // End of the loop.
-		?>
+        endwhile; // End of the loop.
+        ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
-	<div class="wrapper" id="page-wrapper">
+    <div class="wrapper" id="page-wrapper">
     <div id="content">
         <main class="site-main" id="main">
             <div class="container">
